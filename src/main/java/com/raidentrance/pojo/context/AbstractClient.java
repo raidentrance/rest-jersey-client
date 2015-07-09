641ea121604c1a904058a4342172cb4a6884041a
Alex <aagapito@neology-rfid.com>
2015-07-09 18:59:12 -0500

    initial commit

---
+++ src/main/java/com/raidentrance/pojo/context/AbstractClient.java
@@ -0,0 +1,45 @@
+package com.raidentrance.pojo.context;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author alex
+ *
+ */
+
+public abstract class AbstractClient {
+	
+	private ServerContext context;
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(AbstractClient.class);
+
+	protected AbstractClient(ServerContext context) {
+		this.context = context;
+	}
+
+	protected WebTarget createClient(String path) {
+		String assembledPath = assembleEndpoint(path);
+		LOG.debug("Endpoint URL: " + assembledPath);
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(assembledPath);
+		if (context.getUsername() != null && context.getPassword() != null) {
+			HttpAuthenticationFeature feature = HttpAuthenticationFeature
+					.basic(context.getUsername(), context.getPassword());
+			target = target.register(feature);
+		}
+		return target;
+	}
+
+	private String assembleEndpoint(String path) {
+		return context.getServerUrl().concat(context.getContextPath())
+				.concat(path);
+	}
+
+}
---
+++ src/main/java/com/raidentrance/pojo/context/RestCall.java
@@ -0,0 +1,13 @@
+package com.raidentrance.pojo.context;
+
+/**
+ * @author alex
+ *
+ */
+public class RestCall extends AbstractClient{
+
+	protected RestCall(ServerContext context) {
+		super(context);
+	}
+
+}
